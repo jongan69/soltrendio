435596ca6329fe133b27a0ae5813da685cf6f8d5
Jonathan Gan <jonathang132298@gmail.com>
2025-01-04 09:06:06 -0500

    Doesnt seem to be tweeting

--- src/pages/api/cron/route.ts
+++ src/pages/api/cron/route.ts
@@ -1,9 +1,25 @@
 import getTrends from '@utils/getTrends';
-import { postTweet } from '../twitter/tweet';
-// import { NextResponse } from 'next/server';
 import { formatTrendsTweet } from '@utils/formatTweet';
+import Twitter from 'twitter-api-v2';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+const client = new Twitter({
+    appKey: process.env.TWITTER_API_KEY!,
+    appSecret: process.env.TWITTER_API_SECRET!,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN!,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+});
+
+export async function postTweet(message: string) {
+    try {
+        await client.v2.tweet(message);
+        console.log('Successfully tweeted:', message);
+    } catch (error) {
+        console.error('Error posting tweet:', error);
+        console.log(error);
+    }
+}
 
 export async function GET(request: Request) {
     try {
