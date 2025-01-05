fad7a08c781aa3a594cdb9b936bdf1b65192e862
Jonathan Gan <jonathang132298@gmail.com>
2025-01-05 12:11:47 -0500

    Going to try catch all api calls

--- src/utils/getTrends.ts
+++ src/utils/getTrends.ts
@@ -1,5 +1,10 @@
 export default async function getTrends() {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stats/getTrends`);
-    const trends = await data.json();
-    return trends;
+    try {   
+        const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stats/getTrends`);
+        const trends = await data.json();
+        return trends;
+    } catch (error) {
+        console.error("Error fetching trends:", error);
+        return [];
+    }
 }
