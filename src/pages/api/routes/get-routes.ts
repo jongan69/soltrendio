4b560f689d3e3a487739d3cb766af9663157d4f4
Jonathan Gan <jonathang132298@gmail.com>
2025-01-25 15:53:18 -0500

    added routes API for finding AMMs for CAs

--- src/pages/api/routes/get-routes.ts
+++ src/pages/api/routes/get-routes.ts
@@ -24,7 +24,8 @@ export default async function handler(
     } catch (error) {
         console.error('Error fetching portfolios:', error);
         return res.status(500).json({
-            error: 'Internal server error while fetching portfolios'
+            error: 'Internal server error while fetching Routes',
+            errorMessage: error
         });
     }
 }
