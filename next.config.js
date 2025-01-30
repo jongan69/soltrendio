ee76032846c1ce36dde2dd2889292a6b14e09f9b
Jonathan Gan <jonathang132298@gmail.com>
2025-01-29 23:44:29 -0500

    ofc depenabot messed things up

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     largePageDataBytes: 128 * 100000, // Increase the limit to ~12.8MB
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig 
