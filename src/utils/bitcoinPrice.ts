3a835a9577be8d931fed8d14a73605e40a491267
Jonathan Gan <jonathang132298@gmail.com>
2025-02-13 16:05:15 -0500

    moving to kraken for BTC price

--- src/utils/bitcoinPrice.ts
+++ src/utils/bitcoinPrice.ts
@@ -1,8 +1,8 @@
 export const fetchBitcoinPrice = async () => {
     try {
-        const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+        const response = await fetch(`https://api.kraken.com/0/public/Ticker?pair=XBTUSD`);
         const data = await response.json();
-        const price = data.bpi.USD.rate;
+        const price = data.result.XXBTZUSD.c[0];
         return price;
     } catch (error) {
         console.error("Error fetching Bitcoin price:", error);
