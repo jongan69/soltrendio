79a75c1ff55321a322463081974759e11a602eb6
Jonathan Gan <jonathang132298@gmail.com>
2025-07-07 07:10:43 -0400

    upgrading

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,5 @@
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    styled: true,
-    // If you change the default themes don't forget to update in globals and theme-toggle files
-    themes: [
-      "night",
-      // "retro",
-
-      // uncomment to enable
-      // "dark",
-      // "cupcake",
-      // "bumblebee",
-      // "emerald",
-      // "corporate",
-      // "synthwave",
-      // "retro",
-      // "valentine",
-      // "halloween",
-      // "garden",
-      // "forest",
-      // "aqua",
-      "lofi",
-      // "pastel",
-      // "fantasy",
-      // "wireframe",
-      // "black",
-      // "dracula",
-    ],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-  },
+  plugins: [require("@tailwindcss/typography")],
 };
